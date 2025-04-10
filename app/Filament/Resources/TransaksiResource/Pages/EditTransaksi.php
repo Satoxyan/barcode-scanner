201a56441b51598f77d6b14001365061b10926a1
Satria <akbarsatria117@gmail.com>
2025-04-10 11:55:46 +0700

    perbaikan halaman edit transaksi

--- app/Filament/Resources/TransaksiResource/Pages/EditTransaksi.php
+++ app/Filament/Resources/TransaksiResource/Pages/EditTransaksi.php
@@ -5,6 +5,11 @@ namespace App\Filament\Resources\TransaksiResource\Pages;
 use App\Filament\Resources\TransaksiResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Forms\Form;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Card;
 
 class EditTransaksi extends EditRecord
 {
@@ -15,5 +20,64 @@ class EditTransaksi extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Grid::make(1)
+                    ->schema([
+                        Card::make([
+                            Repeater::make('barangTransaksi')
+                                ->relationship('barangTransaksi')
+                                ->label('Daftar Barang')
+                                ->schema([
+                                    TextInput::make('nama')
+                                        ->label('Nama Barang')
+                                        ->readOnly()
+                                        ->columnSpan(2),
+
+                                    TextInput::make('harga')
+                                        ->label('Harga')
+                                        ->readOnly(),
+
+                                    TextInput::make('jumlah')
+                                        ->label('Jumlah')
+                                        ->numeric()
+                                        ->required()
+                                        ->reactive()
+                                        ->readOnly()
+                                        ->afterStateUpdated(fn ($state, callable $set, $get) =>
+                                            $set('subtotal', $get('harga') * $state)
+                                        ),
+
+                                    TextInput::make('subtotal')
+                                        ->label('Subtotal')
+                                        ->readOnly(),
+                                ])
+                                ->columns([
+                                    'sm' => 5,
+                                    'md' => 5,
+                                ])
+                                ->defaultItems(0)
+                                ->disableItemCreation()
+                                ->disableItemDeletion(),
+                        ]),
+
+                        Grid::make()
+                            ->schema([
+                                TextInput::make('total_harga')
+                                    ->label('Total Harga')
+                                    ->readOnly()
+                                    ->reactive()
+                                    ->afterStateHydrated(function ($state, callable $set, $get) {
+                                        $total = collect($get('barangTransaksi'))->sum('subtotal');
+                                        $set('total_harga', $total);
+                                    }),
+                            ])
+                            ->columns(1),
+                    ]),
+            ]);
     }
 }
