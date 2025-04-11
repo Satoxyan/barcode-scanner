865ad4ccb23e25559d0ab2a62b180f5351272e7b
Satria <akbarsatria117@gmail.com>
2025-04-11 11:19:05 +0700

    fix bug

--- app/Filament/Resources/TransaksiResource.php
+++ app/Filament/Resources/TransaksiResource.php
@@ -202,7 +202,7 @@ class TransaksiResource extends Resource
     {
         return [
             'index' => Pages\ListTransaksis::route('/'),
-          
+            'create' => Pages\CreateTransaksi::route('/create'),
             'edit' => Pages\EditTransaksi::route('/{record}/edit'),
         ];
     }
