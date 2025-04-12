2ecd9d31e388c0af84f4739cc44dee654a27ef78
Satria <akbarsatria117@gmail.com>
2025-04-12 09:43:29 +0700

    nominal

--- app/Filament/Resources/TransaksiResource.php
+++ app/Filament/Resources/TransaksiResource.php
@@ -165,6 +165,7 @@ class TransaksiResource extends Resource
 
                 Tables\Columns\TextColumn::make('total')
                 ->label('Total Harga')
+                ->money('IDR')
                 ->sortable()
                 ->searchable(),
 
@@ -175,10 +176,12 @@ class TransaksiResource extends Resource
 
             Tables\Columns\TextColumn::make('nominal_uang')
                 ->label('Nominal Uang')
+                ->money('IDR')
                 ->sortable(),
 
             Tables\Columns\TextColumn::make('kembalian')
                 ->label('Kembalian')
+                ->money('IDR')
                 ->sortable(),
 
         ])
